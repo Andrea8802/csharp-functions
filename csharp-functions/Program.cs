10421c85bf7579f0604a196e8af5bcdea995b4b1
Andrea Bonaventura <andreabonaventura802@gmail.com>
2023-04-06 15:48:33 +0200

    function 2

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -4,20 +4,34 @@
     {
         static void Main(string[] args)
         {
-            int[] numeri = { 1, 2, 3, 4, 5 };
-            StampaArray(numeri);
+            //int[] numeri = { 1, 2, 3, 4, 5 };
+            //StampaArray(numeri);
 
-      
-        }
+            int numeroUtente;
 
-        static void StampaArray(int[] array)
-        {
-            Console.WriteLine("I dati nell'array sono: ");
-            for(int i = 0; i < array.Length; i++)
+            Console.WriteLine("Inserisci un numero");
+            while (!int.TryParse(Console.ReadLine(), out numeroUtente))
             {
-                Console.WriteLine(array[i]);
-            }
+                Console.WriteLine("Devi inserire un numero!");
+            };
+
+            int numeroQuadrato = Quadrato(numeroUtente);
+
+            Console.WriteLine($"Il numero: {numeroUtente} Al quadrato diventa: {numeroQuadrato}");
         }
 
+        //static void StampaArray(int[] array)
+        //{
+        //    Console.WriteLine("I dati nell'array sono: ");
+        //    for(int i = 0; i < array.Length; i++)
+        //    {
+        //        Console.WriteLine(array[i]);
+        //    }
+        //}
+
+       static int Quadrato(int numero)
+        {
+            return numero *= numero;
+        }
     }
 }
