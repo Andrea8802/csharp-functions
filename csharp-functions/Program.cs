42575c5721e720f64c58836bfe3be86c9af91490
Andrea Bonaventura <andreabonaventura802@gmail.com>
2023-04-06 15:40:46 +0200

    Aggiungere i file di progetto.

---
+++ csharp-functions/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace csharp_functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeri = { 1, 2, 3, 4, 5 };
+            StampaArray(numeri);
+        }
+
+        static void StampaArray(int[] array)
+        {
+            Console.WriteLine("I dati nell'array sono: ");
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
