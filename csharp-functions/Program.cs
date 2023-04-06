800420d3aa727046cdab9e17741aab298c9551e8
Andrea Bonaventura <andreabonaventura802@gmail.com>
2023-04-06 17:04:39 +0200

    Bonus Done

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -5,18 +5,15 @@
         static void Main(string[] args)
         {
 
-            int[] numeri = { 2, 6, 7, 5, 3, 9 };
-
-
+            int[] numeri = CaricaNumeri();
 
             // Funzione 1 ----------
-            // Console.WriteLine("I dati nell'array sono: ");
-            //StampaArray(numeri);
+            Console.WriteLine("I dati nell'array sono: ");
+            StampaArray(numeri);
 
 
 
             // Funzione 2 ----------
-
             //Console.WriteLine("Array al quadrato:");
 
             //StampaArray(ElevaArrayAlQuadrato(numeri));
@@ -25,15 +22,44 @@
 
             //StampaArray(numeri);
 
+
+
             // Funzione 4 ----------
-            //Console.WriteLine($"La somma dell'array è: {sommaElementiArray(numeri)}");
+            //Console.WriteLine($"La somma dell'array è: {SommaElementiArray(numeri)}");
+
 
 
             // Funzione 5
-            int[] arrQuadrato = ElevaArrayAlQuadrato(numeri);
-            int sommaQuadrato = sommaElementiArray(arrQuadrato);
-            Console.WriteLine($"La somma dei numeri al quadrato è {sommaQuadrato}");
+            //int[] arrQuadrato = ElevaArrayAlQuadrato(numeri);
+            //int sommaQuadrato = SommaElementiArray(arrQuadrato);
+            //Console.WriteLine($"La somma dei numeri al quadrato è {sommaQuadrato}");
 
+        }
+
+        static int[] CaricaNumeri()
+        {
+            int quantità;
+
+            Console.WriteLine("Inserisci la quantità di numeri da inserire...");
+            while (!int.TryParse(Console.ReadLine(), out quantità))
+            {
+                Console.WriteLine("Devi inserire un numero!");
+            }
+
+            int[] numeri = new int[quantità];
+
+            for (int i = 0; i < numeri.Length; i++)
+            {
+                Console.WriteLine("Inserisci un numero:");
+                Console.WriteLine($"Rimanenti: {numeri.Length - i}");
+
+                while (!int.TryParse(Console.ReadLine(), out numeri[i]))
+                {
+                    Console.WriteLine("Devi inserire un numero!");
+                }
+            }
+
+            return numeri;
         }
 
         static void StampaArray(int[] array)
@@ -46,34 +72,34 @@
         }
 
         //static int Quadrato(int numero)
-        // {
-        //     return numero *= numero;
-        // }
+        //{
+        //    return numero *= numero;
+        //}
 
-        static int[] ElevaArrayAlQuadrato(int[] array)
-        {
+        //static int[] ElevaArrayAlQuadrato(int[] array)
+        //{
 
-            int[] arrQuadrato = (int[]) array.Clone();
+        //    int[] arrQuadrato = (int[])array.Clone();
 
-            for (int i = 0; i < array.Length; i++)
-            {
-                arrQuadrato[i] *= arrQuadrato[i];
-            }
+        //    for (int i = 0; i < array.Length; i++)
+        //    {
+        //        arrQuadrato[i] *= arrQuadrato[i];
+        //    }
 
-            return arrQuadrato;
-        }
+        //    return arrQuadrato;
+        //}
 
-        static int sommaElementiArray(int[] array)
-        {
-            int somma = 0;
+        //static int SommaElementiArray(int[] array)
+        //{
+        //    int somma = 0;
 
-            for (int i = 0; i < array.Length; i++)
-            {
-                somma += array[i];
-            }
+        //    for (int i = 0; i < array.Length; i++)
+        //    {
+        //        somma += array[i];
+        //    }
 
-            return somma;
-        }
+        //    return somma;
+        //}
 
 
     }
